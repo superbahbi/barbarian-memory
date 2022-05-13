eb7861f76fe8184329fb2a6f82a5fad9c2b46365
Bobby Lee <superbahbi@users.noreply.github.com>
2022-05-12 19:23:43 -0700

    Added shuffle card functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import { useState } from 'react';
 import './App.css';
-const cardImage = [
+const cardImages = [
   { "src": "/img/Barbarian_icons_01_t.PNG" },
   { "src": "/img/Barbarian_icons_02_t.PNG" },
   { "src": "/img/Barbarian_icons_03_t.PNG" },
@@ -8,10 +9,23 @@ const cardImage = [
   { "src": "/img/Barbarian_icons_06_t.PNG" }
 ]
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5 )
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+  console.log(cards, turns)
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
